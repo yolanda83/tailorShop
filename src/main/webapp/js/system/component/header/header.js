68509fc0ed443da295a5c5989345d5ee26a53a69
a047692839q <a047692839q@s09-pc06.ausias.lan>
2019-01-18 18:46:10 +0100

    footer + header + css

--- src/main/webapp/js/system/component/header/header.js
+++ src/main/webapp/js/system/component/header/header.js
@@ -94,28 +94,24 @@ function js(toolService, sessionService, $scope, $http, $location, $mdDialog) {
 
 
 
+// Navbar Dropdown
+//$scope.cliqui = function () {
+//    $scope.clik.resize(function () {
+//        if ($scope.clik.width() >= 980) {
+//            // Cuando pasas por encima de un elemento, se muestra un menu dropdown
+//            $scope.clik.hover(function () {
+//                self.parent().toggleClass("show");
+//                self.parent().find(".dropdown-menu").toggleClass("show");
+//            });
+//
+//            // Esconde el menu cuando el raton ya no hace hover sobre el elemento
+//            $scope.clik.mouseleave(function () {
+//                self.removeClass("show");
+//            });
+//        }
+//    });
+//};
 
 
 }
 
-        // Navbar Dropdown
-       
-        $scope.cliqui = $(window).resize(function () {
-            if ($(window).width() >= 980) {
-                // Cuando pasas por encima de un elemento, se muestra un menú dropdown
-                $(".navbar .dropdown-toggle").hover(function () {
-                    $(this)
-                            .parent()
-                            .toggleClass("show");
-                    $(this)
-                            .parent()
-                            .find(".dropdown-menu")
-                            .toggleClass("show");
-                });
-
-                // Esconde el menú cuando el ratón ya no hace hover sobre el elemento
-                $(".navbar .dropdown-menu").mouseleave(function () {
-                    $(this).removeClass("show");
-                });
-            }
-        });
