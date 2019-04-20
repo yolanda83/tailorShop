8ddda30e504a5810a7735b42a6f26da581172520
Yolanda <yolita83@gmail.com>
2019-04-20 11:45:58 +0200

    Arreglos menores, cambios textos, mensajes de alerta Contacto

--- src/main/webapp/js/app/contacto/contacto.js
+++ src/main/webapp/js/app/contacto/contacto.js
@@ -2,13 +2,27 @@
 
 'use strict';
 
-moduleContacto.controller('contactoController', ['$scope', '$location', 'toolService',
-    function ($scope, $location, toolService) {
+moduleContacto.controller('contactoController', ['$scope', '$location', 'toolService', "$mdDialog",
+    function ($scope, $location, toolService, $mdDialog) {
 
         $scope.ruta = $location.path();
 
         $scope.isActive = toolService.isActive;
 
+        $scope.enviar = function () {
+            $scope.showAlert('Contacto', 'E-Mail enviado correctamente :)');
+            $location.path('/home');
+        }
 
+        $scope.showAlert = function (titulo, description) {
+            $mdDialog.show(
+                    $mdDialog.alert()
+                    .clickOutsideToClose(false)
+                    .title(titulo)
+                    .textContent(description)
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('OK!')
+                    );
+        };
 
     }]);
